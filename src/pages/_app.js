7ef90e5441ec33ec2ee929a90c1e5c08b0d0988a
nathan <kvngnathan8420@gmail.com>
2023-06-26 03:21:34 +0000

    init commit

---
+++ src/pages/_app.js
@@ -0,0 +1,31 @@
+//components
+import NavBar from "@/components/NavBar";
+//styles
+import "@/styles/globals.css";
+//fonts
+import { Montserrat } from "next/font/google";
+//nextjs components
+import Head from "next/head";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <NavBar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
