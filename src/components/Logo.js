d3a68354568b1cfe36f1ed3948af77a7d63eed67
nathan <kvngnathan8420@gmail.com>
2023-06-27 14:36:47 +0000

    Customize left side of the navbar

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import Link from 'next/link'
+import React from 'react';
+
+//framer motion animation
+import { motion } from 'framer-motion';
+
+const MotionLink  = motion(Link) 
 
 const Logo = () => {
   return (
-    <div>
-        Logo
+    <div className=' flex justify-center items-center mt-2'>
+       <MotionLink href="/"
+          whileHover={{
+            backgroundColor: ["#121212", "rgba(131,58,180,1)" , "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)"],
+            transition:{duration:1, repeat: Infinity}
+        }}
+       className=" h-16 w-16 bg-dark text-light flex justify-center items-center rounded-full">
+            NEA
+       </MotionLink>
     </div>
   )
 }
 
-export default Logo
+export default Logo;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
 import Link from 'next/link';
 import React from 'react';
 
+//components
+import Logo from './Logo';
+import { useRouter } from 'next/router';
+
 //nav links
 const links = [
-  {name : "Home",href : "/"},
-  {name : "About",href : "/about"},
-  {name : "project",href : "/project"},
-  {name : "articles", href : "/articles"}
+  {name : "Home",href : "/" ,className: "mr-4"},
+  {name : "About",href : "/about", className : "mx-4"},
+  {name : "project",href : "/project", className : "mx-4"},
+  {name : "articles", href : "/articles", className : "ml-4"}
 ];
 
 //nav icons
@@ -19,15 +23,36 @@ const iconsLinks = [
   {icons : "T", target : "_blank" , href: "/" },
 ]
 
+//
+const CustomLink = ({ name , href , className }) => {
+
+  const router  = useRouter();
+  console.log(router.asPath === href)
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+        {name}
+
+        <span 
+          className={`h-[1px] bg-black inline-block absolute left-0 -bottom-0.5 
+          group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? "w-full" : "w-0"}`}>
+          &nbsp;
+        </span>
+    </Link>
+  )
+}
+
+
 const NavBar = () => {
   return (
     <header className=" w-full px-32 py-8 font-medium flex justify-between items-center">
         <nav>
             {links.map((link, index)=>(
-              <Link key={index} href={link.href}>{link.name}</Link>
+               <CustomLink key={index} href={link.href} name={link.name} className={link.className} />
+              
             ))}
         </nav>
-        <h2>logo</h2>
+       
         <nav>
              {iconsLinks.map((iconLink, index)=>(
               <Link href={iconLink.href} target={iconLink.target}>  
@@ -35,6 +60,10 @@ const NavBar = () => {
               </Link>
              ))}
         </nav>
+
+        <div className=" absolute left-[50%] top-2 translate-x-[50%]">
+          <Logo />
+        </div>
     </header>
   )
 }
