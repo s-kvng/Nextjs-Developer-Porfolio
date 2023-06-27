79f86c6dc2edeb9a00f3c3dc9cae23bf748042a9
nathan <kvngnathan8420@gmail.com>
2023-06-27 05:06:25 +0000

    Add nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,39 @@
+import Link from 'next/link';
 import React from 'react';
+
+//nav links
+const links = [
+  {name : "Home",href : "/"},
+  {name : "About",href : "/about"},
+  {name : "project",href : "/project"},
+  {name : "articles", href : "/articles"}
+];
+
+//nav icons
+const iconsLinks = [
+  {icons : "T", target : "_blank" , href: "/" },
+  {icons : "T", target : "_blank" , href: "/" },
+  {icons : "T", target : "_blank" , href: "/" },
+  {icons : "T", target : "_blank" , href: "/" },
+  {icons : "T", target : "_blank" , href: "/" },
+  {icons : "T", target : "_blank" , href: "/" },
+]
 
 const NavBar = () => {
   return (
-    <header className=" w-full px-32 py-8 font-medium">
+    <header className=" w-full px-32 py-8 font-medium flex justify-between items-center">
         <nav>
-            kjd
+            {links.map((link, index)=>(
+              <Link key={index} href={link.href}>{link.name}</Link>
+            ))}
+        </nav>
+        <h2>logo</h2>
+        <nav>
+             {iconsLinks.map((iconLink, index)=>(
+              <Link href={iconLink.href} target={iconLink.target}>  
+                  {iconLink.icons}
+              </Link>
+             ))}
         </nav>
     </header>
   )
