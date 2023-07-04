e9db04bcb765a98341d96633fd9236291ca867ff
nathan <kvngnathan8420@gmail.com>
2023-07-04 22:08:39 +0000

    Add biography

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,34 +1,58 @@
-import AnimatedText from "@/components/AnimatedText"
-import Layout from "@/components/Layout"
-import Head from "next/head"
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
 
+//
+import Image from "next/image";
+
+//image
+import profile from "../../../public/images/profile/developer-pic-2.jpg"
 
 const About = () => {
-
   return (
     <>
-
       <Head>
-         <title>Nathaniel | about</title>
-         <meta name="description"  content="any description" />
+        <title>Nathaniel | about</title>
+        <meta name="description" content="any description" />
       </Head>
 
-      <main className="flex w-full py-32">
+      <main className="flex flex-col w-full py-32">
+        <Layout className=" pt-14  px-32">
+          <AnimatedText text={`Passion Fuels Purpose!`} className="mb-14" />
 
-        <Layout>
+          <div className=" grid grid-cols-8 gap-4">
+            <div className=" col-span-3">
+              <h2 className=" text-lg font-bold mb-4 uppercase">Biography</h2>
+              <p className=" font-medium">
+                Hi, I'm CodeBucks, a web developer and UI/UX designer with a
+                passion for creating beautiful, functional, and user-centered
+                digital experiences. With 4 years of experience in the field. I
+                am always looking for new and innovative ways to bring my
+                clients' visions to life.
+              </p>
+              <p className=" font-medium my-4">
+                I believe that design is about more than just making things look
+                pretty – it's about solving problems and creating intuitive,
+                enjoyable experiences for users.
+              </p>
+              <p className=" font-medium">
+                Whether I'm working on a website, mobile app, or other digital
+                product, I bring my commitment to design excellence and
+                user-centered thinking to every project I work on. I look
+                forward to the opportunity to bring my skills and passion to
+                your next project.
+              </p>
+            </div>
 
-        <AnimatedText text={`Passion Fuels Purpose!`}/>
-
+           
+          </div>
         </Layout>
-
       </main>
-
-
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
 
 /* 
 
