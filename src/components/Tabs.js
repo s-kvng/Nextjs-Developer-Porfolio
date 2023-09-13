da9deb6a45e24af8b088c466d828adfa4e32afb5
s-kvng <kvngnathan8420@gmail.com>
2023-09-13 22:48:06 +0000

    update

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -95,7 +95,7 @@ export default function Tabs({ projPic1 }) {
         id: 2,
         title: "The worst advice we've ever heard about coffee",
         img: Project1,
-        date: "4d ago",
+        date: "16d ago",
         github: 1,
         shareCount: 2,
       },
