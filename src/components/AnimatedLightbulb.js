a265096ac8f38fad5ee1426fbe9d71f8404ec20c
s-kvng <kvngnathan8420@gmail.com>
2024-01-06 11:52:05 +0000

    update home page

--- src/components/AnimatedLightbulb.js
+++ src/components/AnimatedLightbulb.js
@@ -26,7 +26,7 @@ const AnimatedLightbulb = () => {
   const MotionImage = motion(Image);
 
   return (
-    <div className=" absolute md:bottom-0 md:right-7 w-16 right-5 -bottom-96">
+    <div className=" absolute md:bottom-0 md:right-7 w-16 right-5 -bottom-1/4">
       <MotionImage
         src={lightBulb}
         alt="s-kvng"
