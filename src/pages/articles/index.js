ab5491e2fe2450c6ef8a8333138ad537d7ae2bdc
s-kvng <kvngnathan8420@gmail.com>
2024-01-07 23:19:12 +0000

    update articles

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -11,7 +11,7 @@ const Articles = () => {
         <title>Nathaniel | Articles</title>
         <meta name="description" content="any description" />
       </Head>
-      <main className="flex flex-col mb-16 items-center justify-center overflow-hidden sm:min-h-[74.9vh] 2xl:min-h-[80.9vh]">
+      <main className="flex flex-col mb-16 items-center justify-center overflow-hidden min-h-[70vh] sm:min-h-[74.9vh] 2xl:min-h-[80.9vh]">
         <Layout className="pt-24">
           <AnimatedText text={"Articles"} />
           <ul>
