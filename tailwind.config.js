a02986600f48e91cf18e47edd44da0c0d136344f
s-kvng <kvngnathan8420@gmail.com>
2024-08-13 15:55:58 +0000

    ui: implement magic ui shimmer button

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,31 @@ module.exports = {
         circularDarkSm:
           "repeating-radial-gradient(rgba(255 ,255 ,255, 0.5) 2px, #1b1b1b 4px , #1b1b1b 50px);",
       },
+      animation: {
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+        slide: "slide var(--speed) ease-in-out infinite alternate",
+      },
+      keyframes: {
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
+      },
     },
   },
   plugins: [],
