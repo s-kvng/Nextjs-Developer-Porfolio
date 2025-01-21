63e1eec071804005175f474550dd14ea7e869619
s-kvng <kvngnathan8420@gmail.com>
2025-01-21 15:16:01 +0000

    some changes

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Tabs from "@/components/Tabs";
 
 import Head from "next/head";
+import { BackgroundGradientAnimation } from "@/components/aceternity-ui/background-gradient-animation";
 
 const Projects = () => {
   return (
@@ -11,9 +12,9 @@ const Projects = () => {
         <title>Nathaniel | Projects</title>
         <meta name="description" content="any description" />
       </Head>
-      <main className="flex flex-col items-center justify-center md:px-32 px-3 w-full pt-24 pb-5  h-[80vh]  bg-white dark:bg-dark">
+      <BackgroundGradientAnimation className="flex flex-col items-center justify-center md:px-32 px-3 w-full pt-28 pb-5  h-[100vh]   dark:bg-dark">
         <Tabs />
-      </main>
+      </BackgroundGradientAnimation>
     </>
   );
 };
